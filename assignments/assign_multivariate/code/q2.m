5af5929cf875ba4ef4e3b2bd2136cec2e34bf68d
Aman Singh <60286652+feyn-aman@users.noreply.github.com>
2020-11-13 20:46:53 +0530

    Update q2.m

--- assignments/assign_multivariate/code/q2.m
+++ assignments/assign_multivariate/code/q2.m
@@ -18,7 +18,7 @@ boxplot(error_mean);
 xlabel('log_{10} N');
 ylabel('Relative Error (mean)');
 
-%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 error_cov = zeros([100,5]);
 figure;
 for power = 1:5
@@ -26,12 +26,34 @@ for power = 1:5
         samples = [D(1,1)^(1/2) * randn([1,10^power]) + mu_p(1);
                    D(2,2)^(1/2) * randn([1,10^power]) + mu_p(2)];
         samples = Q*samples;
-        mean_cov = (samples-sum(samples,2)/10^power)*(samples-sum(samples,2)/10^power).'/10^power;
+        mean_cov = ...
+            (samples-sum(samples,2)/10^power)...
+            *(samples-sum(samples,2)/10^power).'/10^power;
         error_cov(iter,power)=...
-            sqrt(sum((mean_cov-[1.6250, -1.9486;-1.9486, 3.8750]).^2, 'all')...
+            sqrt(sum((mean_cov-[1.6250, -1.9486;-1.9486, 3.8750]).^2,...
+            'all')...
             /sum(([1.6250, -1.9486;-1.9486, 3.8750]).^2, 'all'));
     end
 end
 boxplot(error_cov);
 xlabel('log_{10} N');
 ylabel('Relative Error (covariance)');
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+for power = 1:5
+    samples = [D(1,1)^(1/2) * randn([1,10^power]) + mu_p(1);
+               D(2,2)^(1/2) * randn([1,10^power]) + mu_p(2)];
+    samples = Q*samples;
+    figure;
+    scatter(samples(1,:),samples(2,:),'.');
+    hold on;
+    xlabel('X_1'); ylabel('X_2');
+    title(sprintf('N = 10^%d',power));
+    mean = sum(samples,2)/10^power;
+    [Q_emp,D_emp] = eig((samples-mean)*((samples-mean).')/10^power);
+    [~,idx] = sort(diag(D_emp),'descend');
+    D_emp = D_emp(idx,idx); Q_emp = Q_emp(:,idx);
+    last = mean + D_emp(1,1)*Q_emp(:,1);
+    plot([mean(1),last(1)],[mean(2),last(2)],"LineWidth",1.0);
+    hold off;
+end
