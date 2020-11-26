39c471850d3ec508ec006b114531beb80b2286a5
Aman Singh <60286652+feyn-aman@users.noreply.github.com>
2020-11-26 11:27:59 +0530

    Update q2.m

--- assignments/assign5/code/q2.m
+++ assignments/assign5/code/q2.m
@@ -1,5 +1,5 @@
 clear;clc;
-
+rng(3000);
 Nvals = [ 5, 10, 20, 40, 60, 80, 100, 500, 1000, 10000];
 error_ml = zeros([100,10]);
 error_bay = zeros([100,10]);
@@ -10,17 +10,17 @@ for i = 1:10
    y = -1*log(x)/5;
    alpha = (N+4.5)./(sum(y,2)+1);
    error_ml(:,i) = abs(N./sum(y,2)-5)/5;
-   error_bay(:,i) = abs((N+4.5)./(sum(y,2)+1)-5)/5;
+   error_bay(:,i) = abs((N+5.5)./(sum(y,2)+1)-5)/5;
 end
 
 figure;
 boxplot(error_ml,Nvals);
 xlabel('N');
-ylabel('({λ}-λ_{true})/λ_{true}');
+ylabel('|{λ}-λ_{true}| / λ_{true}');
 title('Maximum likelihood estimate');
 
 figure;
 boxplot(error_bay,Nvals);
 xlabel('N');
-ylabel('({λ}-λ_{true})/λ_{true}');
+ylabel('|{λ}-λ_{true}| / λ_{true}');
 title('Bayesian estimate');
